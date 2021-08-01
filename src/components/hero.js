451e68bdf048eaca1b9365b9d2eae39e2fcedb5a
Henry Weng <henryweng2003@gmail.com>
2021-08-01 02:04:28 -0700

    Link styling, responsiveness

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,20 +4,21 @@ import Typewriter from 'typewriter-effect';
 const Hero = () => {
   return (
   <div className="relative h-screen bg-blue-dark">
-    <div className="text-center sm:text-left absolute top-1/2 px-8 transform -translate-y-1/2">
-      <h1 className="hero-header">Hi, my name is <b>Henry Weng.</b></h1>
+    <div className="text-center sm:text-left absolute top-1/2 px-16 lg:px-40 transform -translate-y-1/2">
+      <h1 className="hero-header">Hey, my name is <b>Henry Weng.</b></h1>
       <div className="justify-center sm:justify-start flex hero-header">
         <h1 className="hero-header">
-          I'm&nbsp;
+          I&nbsp;
         </h1>
         <b className="my-0 sm:my-1 lg:my-2">
           <Typewriter
               options={{
                 strings: [
-                  'a developer.', 
-                  'a student.', 
-                  'an engineer.', 
-                  'an entrepreneur.'
+                  'make cool things.',
+                  'solve problems.', 
+                  'love learning.', 
+                  'develop apps.', 
+                  'lead people.',
                 ],
                 autoStart: true,
                 loop: true,
@@ -29,7 +30,14 @@ const Hero = () => {
         </b>
       </div><br />
       <div className="flex">
-       <p className="sm:w-2/3 sm:max-w-4xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur officiis soluta pariatur enim, at laboriosam id dolores animi sit a hic cumque numquam accusamus officia corrupti est, totam praesentium. Labore.</p>
+       <p className="sm:w-2/3 sm:max-w-4xl">
+         I'm a freshman at Stanford University pursuing
+         a degree in computer science and economics. I'm also the co-founder 
+         and chairman of the <a className="link" href="theyei.org">
+           Youth Economics Initiative
+           </a>,
+         a network of economics clubs in 60+ schools serving over 2,500 students.
+       </p>
       </div>
       <button className="outline-button mt-8 sm:mt-14">
         Get in touch
