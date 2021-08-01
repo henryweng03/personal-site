d8a7054e5f43f45dfda796c5d279fbfd11a2cd21
Henry Weng <henryweng2003@gmail.com>
2021-08-01 03:21:54 -0700

    Align absolute center

--- src/components/hero.js
+++ src/components/hero.js
@@ -17,7 +17,7 @@ const Hero = () => {
       <li></li>
       <li></li>
     </ul>
-    <div className="text-center px-7 sm:text-left sm:px-16 absolute top-1/2 lg:px-40 transform -translate-y-1/2">
+    <div className="text-center sm:text-left px-7 absolute top-1/2 md:left-3/10 transform -translate-y-1/2 md:-translate-x-3/10">
       <h1 className="hero-header">Hey, my name is <b>Henry Weng.</b></h1>
       <div className="justify-center sm:justify-start flex hero-header">
         <h1 className="hero-header">
@@ -47,14 +47,14 @@ const Hero = () => {
        <p className="sm:w-2/3 sm:max-w-4xl">
          I'm a freshman at Stanford University pursuing
          a degree in computer science and economics. I'm also the co-founder 
-         and chairman of the <a className="link" href="theyei.org">
+         of the <a className="link" href="theyei.org">
            Youth Economics Initiative
            </a>,
          a network of economics clubs in 60+ schools serving over 2,500 students.
        </p>
       </div>
       <button className="outline-button mt-8 sm:mt-14">
-        Get in touch
+        Get in touch →
       </button>
     </div>
   </div>
