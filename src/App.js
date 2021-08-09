9f52a9f8ad849e645648e7a496206620cecd24ff
Henry Weng <henryweng2003@gmail.com>
2021-08-09 02:39:03 -0700

    Changed website name

--- src/App.js
+++ src/App.js
@@ -6,29 +6,39 @@ import Projects from "./sections/projects"
 import Education from "./sections/education";
 import Skills from "./sections/skills"
 
+
+
 function App() {
+  document.title = "Henry Weng"
   return (
     <div className = "bg-blue-dark">
       <Header />
       <Hero />
+
       <div id="experience">
         <Experience />
       </div>
-      <div className="h-12 md:h-24 lg:h-36 bg-blue-dark"></div>
+      <div className="h-12 md:h-24 lg:h-36 bg-blue-dark"/>
+
       <div id="projects">
         <Projects />
       </div>
-      <div className="h-12 md:h-24 lg:h-36 bg-blue-dark"></div>
+      <div className="h-12 md:h-24 lg:h-36 bg-blue-dark"/>
+      {/* Hover outline is 6 */}
       <div className="-mt-6"/>
+      {/* Reverses the extra div in projects */}
       <div className="-mt-12"/>
+
       <div id="education">
         <Education />
       </div>
-      <div className="h-12 md:h-24 lg:h-36 bg-blue-dark"></div>
+      <div className="h-12 md:h-24 lg:h-36 bg-blue-dark"/>
+
       <div id="skills">
         <Skills />
       </div>
-      <div className="h-24 md:h-36 lg:h-48 bg-blue-dark"></div>
+      <div className="h-24 md:h-36 lg:h-48 bg-blue-dark"/>
+
       <Footer/>
     </div>
   );
