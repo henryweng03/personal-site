b3d8c956815498ed84cba3412fa468968cb702dd
Henry Weng <henryweng2003@gmail.com>
2021-08-28 22:44:58 -0700

    Fixed mobile formatting, updated links

--- src/sections/footer.js
+++ src/sections/footer.js
@@ -11,22 +11,23 @@ const Footer = () => {
         <p className = "font-sfmono text-xs lg:text-sm text-white">© 2021 HENRY WENG</p>
       </div>
       <div className = "flex justify-end items-center space-x-4">
-        <a className="pb-5 sm:py-6" href="mailto:hweng@stanford.edu">
+        <a className="sm:py-6" href="mailto:hweng@stanford.edu">
           <p className = "footer-link font-bold">
             Email
           </p>
         </a>
-        <a className="pb-5 sm:py-6" href="https://www.linkedin.com/in/henry-weng/">
+        <a className="sm:py-6" href="https://www.linkedin.com/in/henry-weng/">
           <p className = "footer-link font-bold">
             LinkedIn
           </p>
         </a>
-        <a className="pb-5 sm:py-6" href="https://github.com/henryweng03">
+        <a className="sm:py-6" href="https://github.com/henryweng03">
           <p className = "footer-link font-bold sm:mr-6">
             GitHub
           </p>
         </a>
       </div>
+      <div className = "pb-6 sm:pb-0"/>
     </div>
   )
 }
