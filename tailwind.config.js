42ab59bb683a7b19cbb2e21520279fe5cfb1bc79
Henry Weng <henryweng03@gmail.com>
2024-01-06 22:20:18 -0800

    More modular color names: Blue -> primary, emerald -> secondary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,46 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        'inter': ["Inter", "Segoe UI"],
-        'sfmono': ["SFMono"],
-        'messinasans': ["MessinaSans", "Segoe UI"],
+        inter: ["Inter", "Segoe UI"],
+        sfmono: ["SFMono"],
+        messinasans: ["MessinaSans", "Segoe UI"],
       },
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        blue: {
-          light: '#7396D3',
-          DEFAULT: '#3B69BA',
-          dark: '#223D6D',
+        transparent: "transparent",
+        current: "currentColor",
+        primary: {
+          light: "#7396D3",
+          DEFAULT: "#3B69BA",
+          dark: "#223D6D",
         },
         purple: {
-          light: '#b6a3f5',
-          DEFAULT: '#866FF7',
-          vibrant: '#4016ca',
-          dark: '#5E49A3'
+          light: "#b6a3f5",
+          DEFAULT: "#866FF7",
+          vibrant: "#4016ca",
+          dark: "#5E49A3",
         },
-        emerald: {
-          light: '#92DDAB',
-          DEFAULT: '#2EB263',
-          dark: '#226D3A',
-        }
+        secondary: {
+          light: "#92DDAB",
+          DEFAULT: "#2EB263",
+          dark: "#226D3A",
+        },
       },
       boxShadow: {
-        green : '0 0px 15px 0px rgba(80, 200, 120, 0.7)'
+        green: "0 0px 15px 0px rgba(80, 200, 120, 0.7)",
       },
       height: {
-        screenBigger: '125vh'
+        screenBigger: "125vh",
       },
       listStyleType: {
-        square: 'square',
-      }
+        square: "square",
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
