87a07ccd4b24072203ad9739a70c49ceee4ebcda
Henry Weng <henryweng03@gmail.com>
2024-02-14 00:18:23 -0800

    Update footer links to icons

--- src/sections/footer.js
+++ src/sections/footer.js
@@ -1,28 +1,55 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FiMail } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="h-auto flex flex-col items-center sm:grid grid-cols-2">
-      <div className="py-2 sm:py-6 sm:ml-6">
+    <div className="h-auto flex flex-col items-center sm:grid grid-cols-2 sm:mx-6 mb-6 sm:mb-0">
+      <div className="py-2 sm:py-6">
         <p className="font-sfmono text-xs lg:text-sm text-white">
           © 2024 HENRY WENG
         </p>
       </div>
-      <div className="flex justify-end items-center space-x-4">
-        <a className="sm:py-6" href="mailto:hweng@stanford.edu">
-          <p className="footer-link font-bold">Email</p>
+      <div className="mt-2 sm:mt-0 flex justify-end items-center space-x-4">
+        <a
+          href="https://linkedin.com/in/henry-weng"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin
+            size={28}
+            className="hidden sm:block text-secondary-light hover:text-white duration-150 opacity-40 hover:opacity-80"
+          />
+          <FaLinkedin
+            size={24}
+            className="block sm:hidden text-secondary-light hover:text-white duration-150 opacity-40 hover:opacity-80"
+          />
         </a>
-        <a className="sm:py-6" href="https://www.linkedin.com/in/henry-weng/">
-          <p className="footer-link font-bold">LinkedIn</p>
+        <a
+          href="https://github.com/henryweng03"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub
+            size={28}
+            className="hidden sm:block text-secondary-light hover:text-white duration-150 opacity-40 hover:opacity-80"
+          />
+          <FaGithub
+            size={24}
+            className="block sm:hidden text-secondary-light hover:text-white duration-150 opacity-40 hover:opacity-80"
+          />
         </a>
-        <a className="sm:py-6" href="https://github.com/henryweng03">
-          <p className="footer-link font-bold sm:mr-6">GitHub</p>
+        <a href="mailto: hweng@stanford.edu">
+          <FiMail
+            size={28}
+            className="hidden sm:block text-secondary-light hover:text-white duration-150 opacity-40 hover:opacity-80"
+          />
+          <FiMail
+            size={24}
+            className="block sm:hidden text-secondary-light hover:text-white duration-150 opacity-40 hover:opacity-80"
+          />
         </a>
       </div>
-      <div className="pb-6 sm:pb-0" />
     </div>
   );
 };
