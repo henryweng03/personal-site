b960cfa51e70244dfe5908df24a7c6e774ba43e6
Henry Weng <henryweng03@gmail.com>
2024-03-05 20:33:18 -0800

    Fix bug where the flag appears a bottom left sometimes for HoverWrapper

--- src/components/HoverWrapper.js
+++ src/components/HoverWrapper.js
@@ -1,25 +1,11 @@
 import React, { useState, useRef, useCallback, useEffect } from "react";
 
 const HoverWrapper = ({ children, hoverComponent: HoverComponent }) => {
-  // State to track whether the mouse is hovering over the child component
   const [isHovering, setIsHovering] = useState(false);
-
-  // Refs to store references to the child component and hover component
   const childRef = useRef(null);
   const hoverRef = useRef(null);
 
-  // Event handler for mouse enter event
-  const handleMouseEnter = useCallback(() => {
-    setIsHovering(true);
-  }, []);
-
-  // Event handler for mouse leave event
-  const handleMouseLeave = useCallback(() => {
-    setIsHovering(false);
-  }, []);
-
-  // Event handler for mouse move event
-  const handleMouseMove = useCallback((event) => {
+  const calculateHoverPosition = useCallback((event) => {
     const childRect = childRef.current.getBoundingClientRect();
     const offsetX = event.clientX - childRect.left;
     const offsetY = event.clientY - childRect.top;
@@ -32,7 +18,25 @@ const HoverWrapper = ({ children, hoverComponent: HoverComponent }) => {
     }
   }, []);
 
-  // Effect to add and remove event listeners
+  const handleMouseEnter = useCallback(
+    (event) => {
+      setIsHovering(true);
+      calculateHoverPosition(event);
+    },
+    [calculateHoverPosition]
+  );
+
+  const handleMouseLeave = useCallback(() => {
+    setIsHovering(false);
+  }, []);
+
+  const handleMouseMove = useCallback(
+    (event) => {
+      calculateHoverPosition(event);
+    },
+    [calculateHoverPosition]
+  );
+
   useEffect(() => {
     const childElement = childRef.current;
 
@@ -46,20 +50,6 @@ const HoverWrapper = ({ children, hoverComponent: HoverComponent }) => {
       childElement.removeEventListener("mousemove", handleMouseMove);
     };
   }, [handleMouseEnter, handleMouseLeave, handleMouseMove]);
-
-  // Effect to set the initial position of the hover component
-  useEffect(() => {
-    if (isHovering && hoverRef.current) {
-      const childRect = childRef.current.getBoundingClientRect();
-      const offsetX = childRect.width / 2;
-      const offsetY = childRect.height / 2;
-
-      hoverRef.current.style.left = `${offsetX}px`;
-      hoverRef.current.style.top = `${
-        offsetY - hoverRef.current.offsetHeight - 2
-      }px`;
-    }
-  }, [isHovering]);
 
   return (
     <div ref={childRef} className="relative inline-block">
